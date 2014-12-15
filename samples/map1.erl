0dbc992411e6a8209a89b54a3a093c36077f19d1
MattiasW <mattiasw@gmail.com>
2014-12-15 09:49:08 +0100

    Investigate how Erlang Core handles R17 maps. There are special core instructions or patterns for this.

--- samples/map1.erl
+++ samples/map1.erl
@@ -1,3 +1,15 @@
 -module(map1).
 
 -compile(export_all).
+
+create1() ->
+    #{age => 10}.
+
+change1(M) ->
+    M#{age := 20}.
+
+upsert1(M) ->
+    M#{name => "mattias"}.
+
+access1(#{age := Age} = Person) ->
+    Age.

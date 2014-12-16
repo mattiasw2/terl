1686e2a4109aa1e078369c36fe1a7e05ccfb5f2d
MattiasW <mattiasw@gmail.com>
2014-12-16 19:57:07 +0100

    Trying to understand the type info in the Erlang Core structure

---
+++ samples/poly1.erl
@@ -0,0 +1,42 @@
+%%%%
+
+-module(poly1).
+
+-compile(export_all).
+
+list1(L) ->
+    lists:map(fun(E) -> E end, L).
+
+-spec map(Fun, List1) -> List2 when
+      Fun :: fun((A) -> B),
+      List1 :: [A],
+      List2 :: [B],
+      A :: term(),
+      B :: term().
+
+map(F, [H|T]) ->
+    [F(H)|map(F, T)];
+map(F, []) when is_function(F, 1) -> [].
+
+type1() ->
+    [{{map,2},
+      [{type,8,bounded_fun,
+        [{type,8,'fun',
+          [{type,8,product,[{var,8,'Fun'},{var,8,'List1'}]},
+           {var,8,'List2'}]},
+         [{type,9,constraint,
+           [{atom,9,is_subtype},
+            [{var,9,'Fun'},
+             {type,9,'fun',
+              [{type,9,product,[{var,9,'A'}]},{var,9,'B'}]}]]},
+          {type,10,constraint,
+           [{atom,10,is_subtype},
+            [{var,10,'List1'},{type,10,list,[{var,10,'A'}]}]]},
+          {type,11,constraint,
+           [{atom,11,is_subtype},
+            [{var,11,'List2'},{type,11,list,[{var,11,'B'}]}]]},
+          {type,12,constraint,
+           [{atom,12,is_subtype},[{var,12,'A'},{type,12,term,[]}]]},
+          {type,13,constraint,
+           [{atom,13,is_subtype},
+            [{var,13,'B'},{type,13,term,[]}]]}]]}]}].
--- src/te_experiments.erl
+++ src/te_experiments.erl
@@ -28,10 +28,65 @@ read(std, Print) ->
 %% not working, create core file instead like this:
 %%     X = compile:file("std.erl",[to_core]).
 %%
-read2(std, Print) ->
-    {ok, _, R} = ec_compile:erl_source_to_core_ast("std.erl"),
-    true = cerl:is_c_module(R),
+read2(File, Print) ->
+    {ok, _, R} = ec_compile:erl_source_to_core_ast(File),
+    %% true = cerl:is_c_module(R),
     case Print of
         true  -> core_pp:format(R), io:write(R);
         false -> R
     end.
+
+%%% trying to understand the core
+-spec t1(string()) -> term().
+t1(File) ->
+    {ok, Module} = dialyzer_utils:get_core_from_src(File),
+    _Name = Module#c_module.name,
+    _Exports = Module#c_module.exports,
+    Attrs = Module#c_module.attrs,
+    _Defs = Module#c_module.defs,
+    get_spec_and_types(Attrs).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% get specs and types without the c_literal stuff
+get_spec_and_types(Attrs) ->
+    L = lists:map(fun(Attr) -> get_spec_or_type(Attr) end, Attrs),
+    L2 = lists:map(fun to_ocaml/1, L),
+    L2.
+
+%%% why is Type a list of 1 element? Can you group several -spec into one -spec?
+get_spec_or_type({#c_literal{anno = _Anno,val = spec}, #c_literal{anno = _Anno2,val = [Type]}}) ->
+    {spec, Type};
+get_spec_or_type({#c_literal{anno = _Anno,val = type}, #c_literal{anno = _Anno2,val = [Type]}}) ->
+    {type, Type};
+get_spec_or_type({#c_literal{anno = _Anno,val = export_type}, #c_literal{anno = _Anno2,val = Types}}) ->
+    %% -export_type([file_contract/0, plt_contracts/0]).
+    {export_type, Types}.
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% convert record to ocaml
+to_ocaml({type, {{record, _Name},_RecordType,_Fields} = Record}) -> Record;
+to_ocaml({export_type, Types} = Record) -> Record;
+to_ocaml({type, {_,_,_} = NamedType}) -> NamedType;
+to_ocaml({spec, {{F,N}, [Type]}}) -> {spec, F, N, Type}.
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% eunit tests
+
+a_test() ->
+    te_experiments:t1("../samples/access1.erl").
+
+b_test() ->
+    te_experiments:t1("../samples/typed_map1.erl").
+
+c_test() ->
+    te_experiments:t1("../samples/map1.erl").
+
+d_test() ->
+    te_experiments:t1("../samples/poly1.erl").
+
+e_test() ->
+    te_experiments:t1("/home/mattias/erl-src/otp/lib/dialyzer/src/dialyzer_contracts.erl").

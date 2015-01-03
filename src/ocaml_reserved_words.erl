b7ecf4292b7994f9dfd106430ac09b5c38388855
MattiasW <mattiasw@gmail.com>
2015-01-03 11:25:15 +0100

    Added the list of all ocaml operators

--- src/ocaml_reserved_words.erl
+++ src/ocaml_reserved_words.erl
@@ -1,6 +1,6 @@
 -module(ocaml_reserved_words).
 
--export([reserved_words/0, operators/0]).
+-export([reserved_words/0, symbols/0, operators/0]).
 
 %% ocaml 4.02 reservered words
 %%
@@ -82,7 +82,7 @@ reserved_words() ->
      "while",
      "with"].
 
-operators() ->
+symbols() ->
     [
      {op, "!=", "neq"},
      {op, "#", "hash"},
@@ -126,4 +126,91 @@ operators() ->
      {op, "||", "vertvert"},
      {op, "}", "rcurly"},
      {op, "~", "approx"}
+    ].
+
+%% ------------------------------------------------------
+%% |    Operator     |         Initial meaning          |
+%% ------------------------------------------------------
+%% | +               |Integer addition.                 |
+%% |- (infix)        |Integer subtraction.              |
+%% |~-   - (prefix)  |Integer negation.                 |
+%% |*                |Integer multiplication.           |
+%% |/                |Integer division. Raise           |
+%% |                 |Division_by_zero if second        |
+%% |                 |argument is zero.                 |
+%% |mod              |Integer modulus. Raise            |
+%% |                 |Division_by_zero if second        |
+%% |                 |argument is zero.                 |
+%% |land             |Bitwise logical "and" on integers.|
+%% |                 |                                  |
+%% |lor              |Bitwise logical "or" on integers. |
+%% |lxor             |Bitwise logical "exclusive or" on |
+%% |                 |integers.                         |
+%% |lsl              |Bitwise logical shift left on     |
+%% |                 |integers.                         |
+%% |lsr              |Bitwise logical shift right on    |
+%% |                 |integers.                         |
+%% |asr              |Bitwise arithmetic shift right on |
+%% |                 |integers.                         |
+%% |+.               |Floating-point addition.          |
+%% |-. (infix)       |Floating-point subtraction.       |
+%% |~-.   -. (prefix)|Floating-point negation.          |
+%% |*.               |Floating-point multiplication.    |
+%% |/.               |Floating-point division.          |
+%% |**               |Floating-point exponentiation.    |
+%% |@                |List concatenation.               |
+%% |^                |String concatenation.             |
+%% |!                |Dereferencing (return the current |
+%% |                 |contents of a reference).         |
+%% |:=               |Reference assignment (update the  |
+%% |                 |reference given as first argument |
+%% |                 |with the value of the second      |
+%% |                 |argument).                        |
+%% |=                |Structural equality test.         |
+%% |<>               |Structural inequality test.       |
+%% |==               |Physical equality test.           |
+%% |!=               |Physical inequality test.         |
+%% |<                |Test "less than".                 |
+%% |<=               |Test "less than or equal".        |
+%% |>                |Test "greater than".              |
+%% |>=               |Test "greater than or equal".     |
+%% |&&   &           |Boolean conjunction.              |
+%% |||   or          |Boolean disjunction.              |
+%% ------------------------------------------------------
+
+
+operators() ->
+    [
+     {op,"+","plus"},          % | + |Integer addition. |
+     {op,"-","minus"},         % |- (infix) |Integer subtraction. |
+     {op,"~-","uminus"},       % |~- - (prefix) |Integer negation. |
+     {op,"*","times"},         % |* |Integer multiplication. |
+     {op,"/","div"},           % |/ |Integer division. Raise |
+     {op,"mod","(mod)"},       % |mod |Integer modulus. Raise |
+     {op,"land","(land)"},     % |land |Bitwise logical "and" on integers.|
+     {op,"lor","(lor)"},       % |lor |Bitwise logical "or" on integers. |
+     {op,"lxor","(lxor)"},     % |lxor |Bitwise logical "exclusive or" on |
+     {op,"lsl","(lsl)"},       % |lsl |Bitwise logical shift left on |
+     {op,"lsr","(lsr)"},       % |lsr |Bitwise logical shift right on |
+     {op,"asr","(asr)"},       % |asr |Bitwise arithmetic shift right on |
+     {op,"+.","fplus"},        % |+. |Floating-point addition. |
+     {op,"-.","fminus"},       % |-. (infix) |Floating-point subtraction. |
+     {op,"~-.","fuminus"},     % |~-. -. (prefix)|Floating-point negation. |
+     {op,"*.","ftimes"},       % |*. |Floating-point multiplication. |
+     {op,"/.","fdiv"},         % |/. |Floating-point division. |
+     {op,"**","fexp"},         % |** |Floating-point exponentiation. |
+     {op,"@","list_append"},   % |@ |List concatenation. |
+     {op,"^","string_append"}, % |^ |String concatenation. |
+     {op,"!","deref"},         % |! |Dereferencing (return the current |
+     {op,":=","assign"},       % |:= |Reference assignment (update the |
+     {op,"=","eq"},            % |= |Structural equality test. |
+     {op,"<>","neq"},          % |<> |Structural inequality test. |
+     {op,"==","eq"},           % |== |Physical equality test. |
+     {op,"!=","neq"},          % |!= |Physical inequality test. |
+     {op,"<","le"},            % |< |Test "less than". |
+     {op,"<=","leq"},          % |<= |Test "less than or equal". |
+     {op,">","gr"},            % |> |Test "greater than". |
+     {op,">=","geq"},          % |>= |Test "greater than or equal". |
+     {op,"&&","band"},         % |&& & |Boolean conjunction. |
+     {op,"||","bor"}           % ||| or |Boolean disjunction. |
     ].
